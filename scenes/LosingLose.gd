fa5a51fe316b1957427fa131fc55e5abdc12844f
Hiruma <kirene.naomikp@gmail.com>
2025-03-19 22:05:22 +0700

    tutorial-4

---
+++ scenes/LosingLose.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var sceneName: String = "LoseScreen"
+
+func _on_Hit_Trigger_body_entered(body):
+
+	if body.get_name() == "Player":
+		
+		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+	
