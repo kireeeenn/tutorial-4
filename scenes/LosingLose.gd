ca5c06480ca2d4a6e356d4278cd535a8f0ed94bb
Hiruma <kirene.naomikp@gmail.com>
2025-03-20 04:14:53 +0700

    tutorial-6 gdformated

--- scenes/LosingLose.gd
+++ scenes/LosingLose.gd
@@ -7,11 +7,11 @@ func _on_Hit_Trigger_body_entered(body):
 	var current_scene = get_tree().get_current_scene().get_name()
 	if body.get_name() == "Player":
 		if current_scene == sceneName:
-			Global.lives -=1
+			Global.lives -= 1
 			print(Global.lives)
-		if (Global.lives == 0):
+		if Global.lives == 0:
 			get_tree().change_scene_to_file(str("res://scenes/GameOver.tscn"))
 		else:
-			get_tree().call_deferred("change_scene_to_file",(str("res://scenes/" + sceneName + ".tscn")))
-
-		
+			get_tree().call_deferred(
+				"change_scene_to_file", str("res://scenes/" + sceneName + ".tscn")
+			)
--- scenes/hp.gd
+++ scenes/hp.gd
@@ -1,4 +1,5 @@
 extends Label
 
+
 func _process(delta):
 	self.text = "Lives : " + str(Global.lives)
