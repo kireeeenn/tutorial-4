bcf7d94172a0663df3e2e84fb2b809ce3fd55a71
Hiruma <kirene.naomikp@gmail.com>
2025-03-20 04:11:40 +0700

    tutorial-6 DONE

--- scenes/LosingLose.gd
+++ scenes/LosingLose.gd
@@ -1,8 +1,17 @@
 extends Area2D
 
-@export var sceneName: String = "LoseScreen"
+@export var sceneName: String = "Level1"
 
 
 func _on_Hit_Trigger_body_entered(body):
+	var current_scene = get_tree().get_current_scene().get_name()
 	if body.get_name() == "Player":
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+		if current_scene == sceneName:
+			Global.lives -=1
+			print(Global.lives)
+		if (Global.lives == 0):
+			get_tree().change_scene_to_file(str("res://scenes/GameOver.tscn"))
+		else:
+			get_tree().call_deferred("change_scene_to_file",(str("res://scenes/" + sceneName + ".tscn")))
+
+		
